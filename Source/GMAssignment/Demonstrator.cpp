4203221866f1682c12bebed05142fdb20a8defa4
Play4leftovers <30878484+Play4leftovers@users.noreply.github.com>
2023-11-16 10:49:07 +0100

    Initial Commit

---
+++ Source/GMAssignment/Demonstrator.cpp
@@ -0,0 +1,9 @@
+#include "Demonstrator.h"
+
+// Sets default values
+ADemonstrator::ADemonstrator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+}
