d18cf84584b3d426ece8fb8308d139ca7b09236e
Tor Nordmark <30878484+Play4leftovers@users.noreply.github.com>
2023-11-21 20:54:42 +0100

    Commit 4

---
+++ Source/GMAssignment/Interpolation/InterpolatorDemonstrator.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InterpolatorDemonstrator.h"
+
+
+// Sets default values
+AInterpolatorDemonstrator::AInterpolatorDemonstrator()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AInterpolatorDemonstrator::BeginPlay()
+{
+	Super::BeginPlay();
+	Origin = GetActorLocation();
+	Goal = Origin + TargetPosition;
+}
+
+// Called every frame
+void AInterpolatorDemonstrator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	Time += DeltaTime;
+	Time = FMath::Clamp(Time, 0, Duration);
+	T = Time / Duration;
+	CurrentValue = Curve->FloatCurve.Eval(T);
+	
+	const auto MoveVector = FMath::Lerp(Origin, Goal, CurrentValue);
+	
+	SetActorLocation(MoveVector);
+
+	if(Time >= Duration)
+	{
+		Reset();
+	}
+}
+
+void AInterpolatorDemonstrator::Reset()
+{
+	Time = 0.f;
+	CurrentValue = 0.f;
+}
+
+
---
+++ Source/GMAssignment/Interpolation/InterpolatorDemonstrator.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GMAssignment/Demonstrator.h"
+#include "InterpolatorDemonstrator.generated.h"
+
+UCLASS()
+class GMASSIGNMENT_API AInterpolatorDemonstrator : public ADemonstrator
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float Duration = 0.f;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UCurveFloat* Curve = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (MakeEditWidget = true))
+	FVector TargetPosition;
+	
+protected:
+
+	float Time = 0.f;
+	float CurrentValue = 0;
+	float T = 0;
+
+	FVector Origin;
+	FVector Goal;
+	
+public:
+	// Sets default values for this actor's properties
+	AInterpolatorDemonstrator();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void Reset();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
